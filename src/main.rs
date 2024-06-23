827fa26b6299192fe4bf6b593908ecb730fdd006
Awad Mackie <firesock.serwalek@gmail.com>
2024-06-23 19:24:35 +0100

    Make discard mode on start optional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use signal_hook::consts::signal;
 use signal_hook::iterator::Signals;
 use std::io::{BufRead, BufWriter, Write};
@@ -6,9 +7,25 @@ use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::Arc;
 use std::thread;
 
+/// Mark and control flows within a pipeflow
+#[derive(Parser)]
+#[command(about)]
+struct Args {
+    /// Start in discard mode
+    ///
+    /// In discard mode, stdin is not passed through. The first signal received
+    /// exits discard mode and passthrough mode resumes.
+    #[arg(short, long, default_value_t = false)]
+    discard_mode_start: bool,
+}
+
 fn reader(tx: Sender<String>, enabled: Arc<AtomicBool>) {
     let stdin = std::io::stdin().lock();
 
+    // We could discard values on stdin when not passthrough without parsing for lines
+    // but then code has to be added to resynchronise to the next/previous delimiter.
+    // Given parsing has to happen during passthrough mode, keep the code simple, but
+    // consider revisiting if CPU consumption in discard mode is a problem
     // TODO: Consider non \n delimiters. \0?
     for line in stdin.lines() {
         match line {
@@ -30,17 +47,18 @@ fn reader(tx: Sender<String>, enabled: Arc<AtomicBool>) {
 
 fn signal_handler(tx: Sender<String>, mut signals: Signals, enabled: Arc<AtomicBool>) {
     for signal in signals.forever() {
-        let send_string = match signal {
-            // TODO: Add other signals + command-line strings
-            signal::SIGUSR1 => {
-                enabled.store(true, Ordering::Relaxed);
-                String::from("===USR1===")
-            }
-            signal::SIGUSR2 => String::from("===USR2==="),
-            _ => panic!("Unhandled signal"),
-        };
+        if !enabled.load(Ordering::Relaxed) {
+            enabled.store(true, Ordering::Relaxed);
+        } else {
+            let send_string = match signal {
+                // TODO: Add other signals + command-line strings
+                signal::SIGUSR1 => String::from("===USR1==="),
+                signal::SIGUSR2 => String::from("===USR2==="),
+                _ => panic!("Unhandled signal"),
+            };
 
-        tx.send(send_string).expect("Writer not available, panic");
+            tx.send(send_string).expect("Writer not available, panic");
+        }
     }
 }
 
@@ -71,13 +89,15 @@ fn writer(rx: Receiver<String>) {
 }
 
 fn main() {
+    let args = Args::parse();
+
     let (tx, rx) = channel();
     let signals =
         Signals::new([signal::SIGUSR1, signal::SIGUSR2]).expect("Unable to register signals");
     let signals_handle = signals.handle();
     let tx_signals = tx.clone();
     // TODO: Add ability to opt-out of this mode
-    let enabled = Arc::new(AtomicBool::new(false));
+    let enabled = Arc::new(AtomicBool::new(!args.discard_mode_start));
     let enabled_signals = Arc::clone(&enabled);
 
     let read = thread::spawn(move || {
